f2bfcb12439234f06efbb2cf940730a724249fe6
grajetapiotr@gmail.com <grajetapiotr@gmail.com>
2020-04-13 22:33:59 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace csharplab06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(184, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.TxtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 38);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(184, 591);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Loading text file: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Three letters: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Two letters: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 641);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace csharplab06
+{
+    public partial class Form1 : Form
+    {
+        List<string> allLinesText;
+        private Dictionary<string, List<string>> threeLetters = new Dictionary<string, List<string>>();
+        private Dictionary<string, List<string>> twoLetters = new Dictionary<string, List<string>>();
+
+        public bool IsProcessed { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();          
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                allLinesText = File.ReadAllLines(@"nazwiska.txt").ToList();
+                sw.Stop();
+                label1.Text += sw.Elapsed.ToString();
+                Load_Three_Letters();
+                Load_Two_Letters();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("The file dosnt exist!", "Problems!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                // Write error.
+                Console.WriteLine(ex);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                MessageBox.Show("The directory doesnt exist!", "Problems!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                // Write error.
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void Load_Two_Letters()
+        {
+            Stopwatch sw = new Stopwatch();
+            Regex regex = new Regex(@"^.* ((.{2}).*)");
+
+            sw.Start();
+            foreach (var name in allLinesText)
+            {
+                string shortName = "";
+                string fullName = "";
+
+                Match match = regex.Match(name);
+
+                if (match.Success)
+                {
+                    shortName = match.Groups[2].Value;
+                    fullName = match.Groups[1].Value;
+                }
+
+                if (twoLetters.ContainsKey(shortName))
+                {
+                    twoLetters[shortName].Add(fullName);
+                }
+                else
+                {
+                    var begins = new List<string>();
+                    begins.Add(fullName);
+                    twoLetters[shortName] = begins;
+                }
+
+            }
+            sw.Stop();
+            label3.Text += sw.Elapsed.ToString();
+        }
+
+        private void Load_Three_Letters()
+        {
+            Stopwatch sw = new Stopwatch();
+            Regex regex = new Regex(@"^.* ((.{3}).*)");
+
+            sw.Start();
+            foreach (var name in allLinesText)
+            {
+                string shortName = "";
+                string fullName = "";
+
+                Match match = regex.Match(name);
+
+                if (match.Success)
+                {
+                    shortName = match.Groups[2].Value;
+                    fullName = match.Groups[1].Value;
+                }
+
+                if (threeLetters.ContainsKey(shortName))
+                {
+                    threeLetters[shortName].Add(fullName);
+                }
+                else
+                {
+                    var begins = new List<string>();
+                    begins.Add(fullName);
+                    threeLetters[shortName] = begins;
+                }
+
+            }
+            sw.Stop();
+            label2.Text += sw.Elapsed.ToString();
+        }
+
+        private void TxtInput_TextChanged(object sender, EventArgs e)
+        {
+            if (this.IsProcessed) return;
+            IsProcessed = true;
+            
+            string value = txtInput.Text;
+            if (value.Length >= 2)
+            {
+                if (twoLetters.ContainsKey(value)){
+                    txtOutput.Clear();
+                    var listOfNames = twoLetters[value];
+                    var i = 0;
+                    foreach (var name in listOfNames)
+                    {
+                        i++;
+                        txtOutput.Text += name + "\r\n";
+                        if (i == 100)
+                            break;
+                    }
+                    
+                }
+                if (value.Length == 3)
+                {
+                    if (threeLetters.ContainsKey(value))
+                    {
+                        txtOutput.Clear();
+                        var listOfNames = threeLetters[value];
+                        var i = 0;
+                        foreach (var name in listOfNames)
+                        {
+                            i++;
+                            txtOutput.Text += name + "\r\n";
+                            if (i == 100)
+                                break;
+                        }
+                    }
+                }
+
+            }
+            IsProcessed = false;
+        }
+
+    }
+}
